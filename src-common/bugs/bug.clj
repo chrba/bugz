a5c5ce09b3e9e7d2b4812a6f7ccefb66e24d4ada
Christian Bannes <cbannes@gmail.com>
2015-01-07 13:44:31 +0100

    remove unused code

--- src-common/bugs/bug.clj
+++ src-common/bugs/bug.clj
@@ -14,23 +14,9 @@
    :orientation orientation
    :waiting? true
    :player? false
-   :food 0
-   :home [26 26]
-   :to-destination pos
-   :spacial-mem {:target-cell nil :visited [] :seen []}
    :last nil
-   ;;:motion :moving
-   ;; :brain {:action 0
-   ;;         :path-following {:target-cell nil :seen [] :visited []}
-   ;;         :target-pos  pos
-   ;;         :target-orientation orientation
-   ;;         :home pos
-   ;;         }
    })
 
-(defn set-destination
-  [bug pos]
-  (assoc bug :to-destination pos))
 
 (defn set-moving
   [bug orientation]
@@ -52,9 +38,7 @@
   (assoc bug :last nil))
 
 
-(defn walk-to-destination
-  [{:keys [delta-time] :as dt} {:keys [to-destination] :as bug}]
-  (assoc (move dt bug to-destination) :last (remove-last bug)))
+
 
 (defn rewind
   [bug]
@@ -72,43 +56,6 @@
             [(+ x  x-change)
              (+ y  y-change)])))
 
-(defn- turn
-  "turns the bug in the given direction. Direction should be :left or :right"
-  [{:keys [orientation] :as bug} direction]
-  (let [change {:left #(- % turn-velocity) :right #(+ % turn-velocity)}]
-    (assoc bug :orientation ((get change direction identity) orientation))))
-
-
-(defn- turn-to-target
-  [{:keys [orientation] :as bug} to-orientation]
-  (let [dist (m/angle-dist orientation to-orientation)]
-    (cond
-     (>= turn-velocity (Math/abs dist)) (assoc bug :orientation to-orientation)
-     (< dist 0) (turn bug :left)
-     :else (turn bug :right))))
-
-(defn- move
-  [{:keys [delta-time] :as dt} {:keys [pos orientation] :as bug} to-pos]
-  (let [
-        vec-to-orientation (map - to-pos pos)
-        to-orientation (m/angle [1 0] vec-to-orientation)
-        turned-bug (turn-to-target bug to-orientation)
-        near-target? (< (m/dist to-pos pos) near-target)
-    ;;    rotate-only? (and (not= orientation to-orientation) (< (m/dist to-pos pos) 5))
-        ]
-    (if near-target?
-      (assoc bug :pos to-pos :waiting? true)
-      (assoc (move-forward dt turned-bug) :waiting? false))))
 
 
 
-(defn carries-food?
-  [{:keys [food] :as bug}]
-  (> food 0))
-
-
-
-(comment (defn go-home
-   [bug]
-   (let [home {:pos (bug :home)}]
-     (move-to-pos bug home))))
